e70a08b39ba7574de6a31bad3338ba6681e873c9
wwtung <wwtung@purdue.edu>
2023-03-23 17:24:34 -0400

    Update files

---
+++ Week1/first_launch.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "This is a one-time installation and setup of the Python and Quarto environments for this NWP course."
+
+# Create a bin directory in the user's home directory
+mkdir ~/.local/bin
+
+# Download and extract Quarto CLI
+cd ~/.local/bin
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.2.313/quarto-1.2.313-linux-rhel7-amd64.tar.gz
+tar -xzvf quarto-1.2.313-linux-rhel7-amd64.tar.gz
+
+# Create a symbolic link to Quarto CLI
+ln -s quarto-1.2.313/bin/quarto quarto
+
+# Add Quarto CLI to the PATH environment variable
+echo "export PATH=\$PATH:~/.local/bin" >> ~/.bashrc
+source ~/.bashrc
+
+# Create directories for NWP course
+mkdir ~/NWP1
+mkdir ~/NWP1/Week1
+mkdir ~/NWP1/Week2
+mkdir ~/NWP1/Week3
+mkdir ~/NWP1/Week4
+mkdir ~/NWP1/Week5
+mkdir ~/NWP1/Capstone
+
+# Load MetPy environment and set kernel for Jupyter Notebook
+module use /anvil/projects/x-ees230003/etc/modules
+module load conda-env/MetPy-py3.8.8
+conda-env-mod kernel -p $PROJECT/apps/MetPy
+
+# Print messages indicating successful installation/setup
+echo "Quarto CLI installed successfully!"
+echo "To Use the Python MetPy environment in the future, type the following commands:"
+echo "module use /anvil/projects/x-ees230003/etc/modules"
+echo "module load conda-env/MetPy-py3.8.8"
+echo "Or, select Python (My MetPy Kernel) in the Jupyter Notebook"
+
--- cloud-paradigm.md
+++
@@ -1,5 +0,0 @@
----
-title: Cloud Paradigm
----
-
-[Slides](https://the-examples-book.com/data-engineering/rcac/anvil-getting-started) that introduce Anvil.
